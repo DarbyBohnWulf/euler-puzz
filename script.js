f4fd6a7c8128245924a6693d0a60b9f6e86d5c15
Mark R <mwritchie2@gmail.com>
2016-03-02 18:26:02 -0600

    solution 2.derp

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var sumUn1000 = function() {
-  for (var i = 0; i < 1000; i += 3);
-  for (var j = 0; j < 1000; j += 5);
+  for (var i = 0; i < 999; i += 3);
+  for (var j = 0; j < 995; j += 5);
   return j + i;
 };
 
